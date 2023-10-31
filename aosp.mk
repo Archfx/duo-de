1dd2fe4a47156bd63305b34413b4fd03fb577994
Alberto Ponces <ponces26@gmail.com>
2023-10-31 12:12:36 +0000

    chore: Migrate to vendor_ponces

--- aosp.mk
+++ aosp.mk
@@ -1,8 +1,1 @@
-$(call inherit-product, device/phh/treble/fonts/fonts.mk)
-$(call inherit-product, device/phh/treble/clocks/clocks.mk)
-
-PRODUCT_PACKAGES += \
-    ThemePicker
-
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.system.ota.json_url=https://raw.githubusercontent.com/ponces/treble_build_aosp/android-14.0/ota.json
+$(call inherit-product, vendor/ponces/config/common.mk)
