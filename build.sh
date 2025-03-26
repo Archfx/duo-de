89415f3605971e724225287657bd0fef09dbc542
Aruna | අරුණ <archfx@duck.com>
2025-03-26 11:18:45 -0400

    Sync with Ponces (bump: up to android-15.0.0_r23)

--- build.sh
+++ build.sh
@@ -11,19 +11,21 @@ echo
 set -e
 
 
-BL=$PWD/treble_aosp
-BD=$PWD/duo-de/builds
-BV=$1
+export BUILD_NUMBER="$(date +%y%m%d)"
+
+BUILD_ROOT="$PWD/treble_aosp"
+BUILD_DIR=$PWD/duo-de/builds
+
 
 initRepos() {
     echo "--> Initializing workspace"
-    repo init -u https://android.googlesource.com/platform/manifest -b android-15.0.0_r10 --git-lfs
+    repo init -u https://android.googlesource.com/platform/manifest -b android-15.0.0_r23 --git-lfs
     echo
 
     echo "--> Preparing local manifest"
     mkdir -p .repo/local_manifests
-    cp $BL/build/default.xml .repo/local_manifests/default.xml
-    cp $BL/build/remove.xml .repo/local_manifests/remove.xml
+    cp $BUILD_ROOT/build/default.xml .repo/local_manifests/default.xml
+    cp $BUILD_ROOT/build/remove.xml .repo/local_manifests/remove.xml
     echo
 }
 
@@ -37,20 +39,20 @@ syncRepos() {
 
 applyPatches() {
     echo "--> Applying TrebleDroid patches"
-    bash $BL/patch.sh $BL trebledroid
+    bash $BUILD_ROOT/patch.sh $BUILD_ROOT trebledroid
     echo
 
     echo "--> Applying personal patches"
-    bash $BL/patch.sh $BL personal
+    bash $BUILD_ROOT/patch.sh $BUILD_ROOT personal
     echo
 
     echo "--> Applying DUO-DE patches"
-    bash $BL/patch.sh $BL duo
+    bash $BUILD_ROOT/patch.sh $BUILD_ROOT duo
     echo
 
     echo "--> Generating makefiles"
     cd device/phh/treble
-    cp $BL/build/aosp.mk .
+    cp $BUILD_ROOT/build/aosp.mk .
     bash generate.sh aosp
     cd ../../..
     echo
@@ -58,8 +60,9 @@ applyPatches() {
 
 setupEnv() {
     echo "--> Setting up build environment"
-    source build/envsetup.sh &>/dev/null
-    mkdir -p $BD
+    mkdir -p $BUILD_DIR
+    source build/envsetup.sh
+    source build/core/build_id.mk
     echo
 }
 
@@ -75,23 +78,23 @@ buildTrebleApp() {
 
 buildVariant() {
     echo "--> Building $1"
-    lunch "$1"-ap4a-userdebug
+    lunch "$1"-bp1a-userdebug
     make -j$(nproc --all) installclean
     make -j$(nproc --all) systemimage
     make -j$(nproc --all) target-files-package otatools
-    bash $BL/sign.sh "../archfx-priv/keys" $OUT/signed-target_files.zip
+    bash $BUILD_ROOT/sign.sh "../archfx-priv/keys" $OUT/signed-target_files.zip
     unzip -jqo $OUT/signed-target_files.zip IMAGES/system.img -d $OUT
-    mv $OUT/system.img $BD/system-"$1".img
+    mv $OUT/system.img $BUILD_DIR/system-"$1".img
 
-    echo "image copied to $BD/system-"$1".img"
+    echo "image copied to $BUILD_DIR/system-"$1".img"
     echo
 }
 
 buildVndkliteVariant() {
     echo "--> Building $1-vndklite"
     cd treble_adapter
-    sudo bash lite-adapter.sh "64" $BD/system-"$1".img
-    mv s.img $BD/system-"$1"-vndklite.img
+    sudo bash lite-adapter.sh "64" $BUILD_DIR/system-"$1".img
+    mv s.img $BUILD_DIR/system-"$1"-vndklite.img
     sudo rm -rf d tmp
     cd ..
     echo
@@ -113,15 +116,15 @@ buildVariants() {
 generatePackages() {
     echo "--> Generating packages"
     buildDate="$(date +%Y%m%d)"
-    find $BD/ -name "system-treble_*.img" | while read file; do
+    find $BUILD_DIR/ -name "system-treble_*.img" | while read file; do
         filename="$(basename $file)"
         [[ "$filename" == *"_a64"* ]] && arch="arm32_binder64" || arch="arm64"
         [[ "$filename" == *"_bvN"* ]] && variant="vanilla" || variant="gapps"
         [[ "$filename" == *"-vndklite"* ]] && vndk="-vndklite" || vndk=""
         name="aosp-${arch}-ab-${variant}${vndk}-15.0-$buildDate"
-        xz -cv "$file" -T0 > $BD/"$name".img.xz
+        xz -cv "$file" -T0 > $BUILD_DIR/"$name".img.xz
     done
-    rm -rf $BD/system-*.img
+    rm -rf $BUILD_DIR/system-*.img
     echo
 }
 
@@ -131,7 +134,7 @@ generateOta() {
     buildDate="$(date +%Y%m%d)"
     timestamp="$START"
     json="{\"version\": \"$version\",\"date\": \"$timestamp\",\"variants\": ["
-    find $BD/ -name "aosp-*-15.0-$buildDate.img.xz" | sort | {
+    find $BUILD_DIR/ -name "aosp-*-15.0-$buildDate.img.xz" | sort | {
         while read file; do
             filename="$(basename $file)"
             [[ "$filename" == *"-arm32"* ]] && arch="a64" || arch="arm64"
@@ -143,13 +146,13 @@ generateOta() {
             json="${json} {\"name\": \"$name\",\"size\": \"$size\",\"url\": \"$url\"},"
         done
         json="${json%?}]}"
-        echo "$json" | jq . > $BL/config/ota.json
+        echo "$json" | jq . > $BUILD_ROOT/config/ota.json
     }
     echo
 }
 
 uploadOTA() {
-    bash $BL/upload.sh
+    bash $BUILD_ROOT/upload.sh
 }
 
 
