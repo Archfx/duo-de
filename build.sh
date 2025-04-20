2ae6114b7193638d176d1a81914a2906e735670b
Archfx <cybx@live.com>
2025-04-20 13:45:42 -0400

    Revert Bump OTA from v2025.04.20 to v2025.03.11

--- build.sh
+++ build.sh
@@ -103,9 +103,10 @@ buildVndkliteVariant() {
 buildVariants() {
     # buildVariant treble_a64_bvN
     # buildVariant treble_a64_bgN
-
-    buildVariant treble_arm64_bvN
+    
     buildVariant treble_arm64_bgN
+    buildVariant treble_arm64_bvN
+    
     
  
     # buildVndkliteVariant treble_a64_bvN
